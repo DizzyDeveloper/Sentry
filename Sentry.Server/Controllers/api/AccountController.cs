fc50456de682c65932d9a9f2ed06e5bfc95d729c
Heinrich <heinrich.strauss@gispeople.com.au>
2018-02-07 12:26:02 +1000

    Created Sentry.Server solution.

---
+++ Sentry.Server/Controllers/api/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sentry.Server.Controllers.api
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult ("Hello World");
+            //return Ok();
+        }
+    }
+}
---
+++ Sentry.Server/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sentry.Server
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore();
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "api/{controller}/{action}/{id?}");
+            });
+
+                app.UseSpa(spa => {
+                spa.Options.SourcePath = "../Sentry.Client";
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer("start");
+                }
+            });
+        }
+    }
+}
