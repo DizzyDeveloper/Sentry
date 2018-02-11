ac12ff3ef9460af20bb4e1379b39c674ee4f7c73
Heinrich <heinrich.strauss@gispeople.com.au>
2018-02-12 07:37:34 +1000

    Initial Configuration setup.

--- Sentry.Server/Startup.cs
+++ Sentry.Server/Startup.cs
@@ -1,16 +1,26 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 
 namespace Sentry.Server
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; private set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = configBuilder.Build();
+
             services.AddMvcCore();
             services.AddSpaStaticFiles(configuration =>
             {
@@ -28,12 +38,12 @@ namespace Sentry.Server
                     template: "api/{controller}/{action}/{id?}");
             });
 
-                app.UseSpa(spa => {
-                spa.Options.SourcePath = "../Sentry.Client";
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer("start");
-                }
+            app.UseSpa(spa => {
+                 spa.Options.SourcePath = "../Sentry.Client";
+                 if (env.IsDevelopment())
+                 {
+                     spa.UseAngularCliServer("start");
+                 }
             });
         }
     }
