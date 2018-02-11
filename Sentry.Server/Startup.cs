c8ca21aaea947508a60df977bd67c44c1a37eabc
Heinrich <heinrich.strauss@gispeople.com.au>
2018-02-12 07:54:20 +1000

    Added initial Autofac support.

--- Sentry.Server/Startup.cs
+++ Sentry.Server/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Autofac;
+
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
@@ -26,6 +28,20 @@ namespace Sentry.Server
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+        }
+
+        // ConfigureContainer is where you can register things directly
+        // with Autofac. This runs after ConfigureServices so the things
+        // here will override registrations made in ConfigureServices.
+        // Don't build the container; that gets done for you. If you
+        // need a reference to the container, you need to use the
+        // "Without ConfigureContainer" mechanism shown later.
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder
+                .RegisterInstance(Configuration)
+                .As<IConfiguration>()
+                .ExternallyOwned();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
